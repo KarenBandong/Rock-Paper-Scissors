828bc4e72104e8335221e7c3c75a2bec1a421214
Karen <karen.bandong.2018@student.lhschools.org>
2019-05-10 19:54:09 -0400

    Boolean running created

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -3,16 +3,17 @@ package org.lickingheights;
 public class Main {
     String[] computerOptions = new String[3];
     String computerChoice;
+    boolean running= true;
 
     public  void start(){
         computerOptions[0] = "Rock";
         computerOptions[1] = "Paper";
         computerOptions[2] = "Scissors";
-    
+
     }
 
     public static void main(String[] args) {
-        String[] choices =["rock","paper","scissors"];
-
+        System.out.println("Let's start the game of Rock, Paper, and Scissors!");
+        while (running)
     }
 }
