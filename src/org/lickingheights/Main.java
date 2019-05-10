fe99dc6df8ff1e714dc505f0b84224d233e77bab
Karen <karen.bandong.2018@student.lhschools.org>
2019-05-10 19:49:03 -0400

    Created New Array

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -1,8 +1,13 @@
 package org.lickingheights;
 
 public class Main {
+    String[] computerOptions = new String[3];
+    String computerChoice;
+
+    
 
     public static void main(String[] args) {
         String[] choices =["rock","paper","scissors"];
+
     }
 }
